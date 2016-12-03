16e04a3b69f26b783c7b8c91e8b357552e32cf36
syh02 <syh02@syhsvn.local>
2016-12-03 11:11:07 +0800

    第一次提交

---
+++ helloworld/helloworld/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  helloworld
+//
+//  Created by syh02 on 16/12/3.
+//  Copyright © 2016年 syh02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        "你好 世界"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
